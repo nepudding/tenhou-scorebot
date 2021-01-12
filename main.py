cc559a6b50bd6a777f25a8f78aa6aa659251bfd1
hash <mizuki.1218zx@gmail.com>
2021-01-12 11:08:22 +0900

    hoge

--- main.py
+++ main.py
@@ -47,4 +47,5 @@ def handle_message(event):
 
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=int(os.getenv("PORT")))
+    port = int(os.getenv("PORT","80"))
+    app.run(host="0.0.0.0", port=port)
