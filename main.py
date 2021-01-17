0ed39f7c5bafd43bebe211f01116beb0908a2eb6
hash <mizuki.1218zx@gmail.com>
2021-01-17 15:13:36 +0900

    hoge

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ def set_score(day, room):
             sql = "INSERT INTO scores (date, user_name, rank, score, room_id) VALUES "
             values = []
             for log in logs:
-                for hoge in log[score]:
+                for hoge in log['score']:
                     name, score = hoge.split(",")
                     values.append(f"('{log['date']}, '{name}', {score}, '{room}')")
             cur.execute(sql + ",".join(values) + ";")
