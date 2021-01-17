accb26e0b49ae652de802c692585d8ee4fbfaa2e
hash <mizuki.1218zx@gmail.com>
2021-01-17 17:41:20 +0900

    hoge

--- main.py
+++ main.py
@@ -60,14 +60,15 @@ def handle_message(event):
     if hoge.startswith("にゃーん"):
         line_bot_api.reply_message(
             event.reply_token,
-            [TextSendMessage("にゃ〜ん"),TextSendMessage("にゃにゃにゃ")]
+            [TextSendMessage("にゃ〜ん"),TextSendMessage("にゃ〜ん"),
+            TextSendMessage("にゃにゃにゃ"),TextSendMessage("にゃにゃにゃ")]
         )
     if hoge.startswith("きろく"):
-        data = aggregate_score()
+        data = map(str, aggregate_score())
         print(data)
         line_bot_api.reply_message(
             event.reply_token,
-            map(TextSendMessage,map(str,data))
+            map(TextSendMessage, data)
         )
     if hoge.startswith("こうしん"):
         _, date, room = hoge.split()
