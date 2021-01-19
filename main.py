f7c9b31a4017197ad9ae6a2ce5e8504991ba7870
hash <hoge@piyo>
2021-01-19 17:35:25 +0900

    コマンドを変更

--- main.py
+++ main.py
@@ -51,12 +51,12 @@ def callback():
 @handler.add(MessageEvent, message=TextMessage)
 def handle_message(event):
     hoge = event.message.text
-    if hoge.startswith("にゃーん"):
+    if hoge.startswith("\にゃーん"):
         line_bot_api.reply_message(
             event.reply_token,
             TextSendMessage("にゃ〜ん")
         )
-    if hoge.startswith("せいせき"):
+    if hoge.startswith("\せいせき"):
         room_id, _ = my_database.current_tournament()
         score = my_database.get_score_sum(room_id)
         text = f"{room_id}"
@@ -68,7 +68,7 @@ def handle_message(event):
             event.reply_token,
             TextSendMessage(text)
         )
-    if hoge.startswith("こうしん"):
+    if hoge.startswith("\こうしん"):
         _, date, taikai = hoge.split()
         sql = f"SELECT room_id FROM tournaments WHERE name='{taikai}'"
         room = my_database.sql_requests(sql)[0][0]
@@ -78,7 +78,7 @@ def handle_message(event):
             TextSendMessage(text=res)
         )
 
-    if hoge.startswith("たいかいとうろく"):
+    if hoge.startswith("\たいかいとうろく"):
         _, name, room, url = hoge.split()
         text = my_database.set_tournament(name, room, url)
         line_bot_api.reply_message(
@@ -86,7 +86,7 @@ def handle_message(event):
             TextSendMessage(text=text)
         )
 
-    if hoge.startswith("ゆーざー"):
+    if hoge.startswith("\ゆーざー"):
         _, nickname, id = hoge.split()
         my_database.set_user(nickname, id)
 
